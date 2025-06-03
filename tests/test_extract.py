f9a3cffd1a6ac6df1d0486014ba7e9463b89667e
Adam Pingel <oss@pingel.org>
2025-06-03 01:07:21 -0400

    one chunk extract per minute
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -157,7 +157,7 @@ def test_enrich():
         extraction_model_id,
         chunk_extraction_template,
         TestDocumentChunkExtractions,
-        delay=5.0,
+        delay=60.0,
         console=console,
     )
 
